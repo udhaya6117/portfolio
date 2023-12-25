9fc03f4c112e4183481bcc7519389a7b36d7b8e4
udhaya <udhayaprakash6117@gmail.com>
2023-12-25 22:06:01 +0530

    git added

--- src/myprofile/Profile.js
+++ src/myprofile/Profile.js
@@ -5,11 +5,10 @@ import Lottie from "lottie-react";
 import animationdata from "./../animations/Animation - 1703388702458.json";
 import animationcircle from "./../IMGS/Yellow Modern Coming Soon Instagram Post (2).png";
 import me from "./../IMGS/potofiolo-removebg-preview.png";
-import EastIcon from '@mui/icons-material/East';
+import EastIcon from "@mui/icons-material/East";
 import { ReactComponent as Waveimg } from "../IMGS/latest.svg";
 
 const Profile = () => {
-    
   return (
     <div className="mainpage">
       <div className="overlay">
@@ -39,14 +38,21 @@ const Profile = () => {
         <Container>
           <div className="hero-context">
             <div className="content-self">
-              <div className="INTRO"><text>HELLO, I'M A</text>
-              <h1>WEB <span>DEVELOPER.</span></h1>
-              <p>HI I'm Udhaya Prakash, a passionate UI Web Developer bassed in the india. welcome to portfolio world.</p>
-              <button className="btn-intro">About Me</button></div>
+              <div className="INTRO">
+                <text>HELLO, I'M A</text>
+                <h1>
+                  WEB <span>DEVELOPER.</span>
+                </h1>
+                <p>
+                  HI I'm Udhaya Prakash, a passionate UI Web Developer bassed in
+                  the india. welcome to portfolio world.
+                </p>
+                <button className="btn-intro">About Me</button>
+              </div>
               <div className="location">
                 <div className="box-location">
-                    <text>India</text>
-                    <text>Tamil Nadu , Chennai</text>
+                  <text>India</text>
+                  <text>Tamil Nadu , Chennai</text>
                 </div>
               </div>
             </div>
@@ -55,14 +61,16 @@ const Profile = () => {
               <div className="secound-img">
                 <img src={me} />
               </div>
-                <text className="text-blob-1">UI</text>
-                <text className="text-blob-2">WEB <br/> DEVELOPER</text>
+              <text className="text-blob-1">UI</text>
+              <text className="text-blob-2">
+                WEB <br /> DEVELOPER
+              </text>
             </div>
           </div>
         </Container>
       </div>
-      <div id="about" className="">
-
+      <div id="about" className="about">
+        afafaf
       </div>
     </div>
   );
